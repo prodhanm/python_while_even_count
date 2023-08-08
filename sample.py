f6347c7eb2d24daf1a35a2c3a26296b31cee9b72
prodhanm <mustafizur.prodhan@gmail.com>
2023-08-08 11:40:44 -0400

    after running the debugger, the code is performing as it should.

---
+++ sample.py
@@ -0,0 +1,13 @@
+def even_ct():
+    even_li = []
+    num = 0
+    while num < 21:
+        even_li.append(num)
+        num += 2
+    print(even_li)
+
+def main():
+    even_ct()
+
+if __name__ == "__main__":
+    main()
